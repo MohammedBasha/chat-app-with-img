dd83c8d117b99d1082643bd70e07c8a7802342b1
MohammedBasha <me@mohammedbasha.com>
2025-11-25 18:20:29 +0200

    FInishing the login, logout, and signup functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
     const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -58,6 +59,8 @@ function App() {
                         }
                     />
                 </Routes>
+
+                <Toaster />
             </div>
         </>
     );
